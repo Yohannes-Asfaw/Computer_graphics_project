7e47f6e0d912ef064cdabf1d11733f77b079757b
Yohannes_Asfaw <jonathanasfaw11@gmail.com>
2021-12-27 17:09:39 +0300

    commiting the texture loader file

---
+++ texture_loader.py
@@ -0,0 +1,39 @@
+from OpenGL.GL import glBindTexture, glTexParameteri, GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, \
+    GL_TEXTURE_WRAP_T, GL_REPEAT, GL_TEXTURE_MIN_FILTER, GL_TEXTURE_MAG_FILTER, GL_LINEAR,\
+    glTexImage2D, GL_RGBA, GL_UNSIGNED_BYTE
+
+from PIL import Image
+
+
+
+def texture_loader(route, textu):
+    glBindTexture(GL_TEXTURE_2D, textu)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    # image load
+    image = Image.open(route)
+    image = image.transpose(Image.FLIP_TOP_BOTTOM)
+    img_data = image.convert("RGBA").tobytes()
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
+    return textu
+
+
+def texture_load_for_pygame(route, textu):
+    import pygame
+    glBindTexture(GL_TEXTURE_2D, textu)
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+
+    image = pygame.image.load(route)
+    image = pygame.transform.flip(image, False, True)
+    image_width, image_height = image.get_rect().size
+    img_data = pygame.image.tostring(image, "RGBA")
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
+    return textu
